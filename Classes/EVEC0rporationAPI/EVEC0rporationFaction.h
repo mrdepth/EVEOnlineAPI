67bbecb2fd4c9cb1b8f47d6cf1dab072a063c0a0
mrdepth <shimanski.artem@gmail.com>
2012-03-23 16:26:25 -0100

    C0rporation API added

---
+++ Classes/EVEC0rporationAPI/EVEC0rporationFaction.h
@@ -0,0 +1,13 @@
+//
+//  EVEC0rporationFaction.h
+//  EVEOnlineAPI
+//
+//  Created by Mr. Depth on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EVEC0rporationFaction : NSObject
+
+@end
---
+++ Classes/EVEC0rporationAPI/EVEC0rporationFaction.m
@@ -0,0 +1,13 @@
+//
+//  EVEC0rporationFaction.m
+//  EVEOnlineAPI
+//
+//  Created by Mr. Depth on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "EVEC0rporationFaction.h"
+
+@implementation EVEC0rporationFaction
+
+@end
