6c02149cbe0ae9a20b5a3b82c725f09e45ef5031
mrdepth <shimanski.artem@gmail.com>
2014-01-13 18:05:43 +0300

    EVEImage Retina support

--- Classes/EVEOnlineAPI/Misc/EVEImage.h
+++ Classes/EVEOnlineAPI/Misc/EVEImage.h
@@ -8,15 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum {
+typedef NS_ENUM(NSInteger, EVEImageSize) {
 	EVEImageSize32 = 32,
 	EVEImageSize64 = 64,
 	EVEImageSize128 = 128,
 	EVEImageSize200 = 200,
 	EVEImageSize256 = 256,
 	EVEImageSize512 = 512,
-	EVEImageSize1024 = 1024
-} EVEImageSize;
+	EVEImageSize1024 = 1024,
+	
+	EVEImageSizeRetina = 0x1000,
+	
+	EVEImageSizeRetina32 = EVEImageSize32 | EVEImageSizeRetina,
+	EVEImageSizeRetina64 = EVEImageSize64 | EVEImageSizeRetina,
+	EVEImageSizeRetina128 = EVEImageSize128 | EVEImageSizeRetina,
+	EVEImageSizeRetina200 = EVEImageSize200 | EVEImageSizeRetina,
+	EVEImageSizeRetina256 = EVEImageSize256 | EVEImageSizeRetina,
+	EVEImageSizeRetina512 = EVEImageSize512 | EVEImageSizeRetina,
+	EVEImageSizeRetina1024 = EVEImageSize1024 | EVEImageSizeRetina,
+	
+};
 
 @interface EVEImage : UIImage {
 }
--- Classes/EVEOnlineAPI/Misc/EVEImage.m
+++ Classes/EVEOnlineAPI/Misc/EVEImage.m
@@ -9,37 +9,40 @@
 #import "EVEImage.h"
 #import "EVEGlobals.h"
 
+@interface EVEImage()
++ (NSInteger) imageDimensionWithSize:(EVEImageSize) size;
+@end
 
 @implementation EVEImage
 
 + (NSURL*) characterPortraitURLWithCharacterID: (NSInteger) characterID size: (EVEImageSize) size error:(NSError **)errorPtr {
-	if (size != EVEImageSize32 && size != EVEImageSize64 && size != EVEImageSize128 && size != EVEImageSize200 && size != EVEImageSize256 && size != EVEImageSize512 && size != EVEImageSize1024) {
+	/*if (size != EVEImageSize32 && size != EVEImageSize64 && size != EVEImageSize128 && size != EVEImageSize200 && size != EVEImageSize256 && size != EVEImageSize512 && size != EVEImageSize1024) {
 		if (errorPtr)
 			*errorPtr = [NSError errorWithDomain:EVEOnlineErrorDomain code:EVEErrorCodeInvalidImageSize userInfo:[NSDictionary dictionaryWithObject:EVEErrorCodeInvalidImageSizeText forKey:NSLocalizedDescriptionKey]];
 		return nil;
 	}
-	else
-		return [NSURL URLWithString:[NSString stringWithFormat:@"http://image.eveonline.com/Character/%d_%d.jpg", characterID, size]];
+	else*/
+		return [NSURL URLWithString:[NSString stringWithFormat:@"http://image.eveonline.com/Character/%d_%d.jpg", characterID, [self imageDimensionWithSize:size]]];
 }
 
 + (NSURL*) corporationLogoURLWithCorporationID: (NSInteger) corporationID size: (EVEImageSize) size error:(NSError **)errorPtr {
-	if (size != EVEImageSize32 && size != EVEImageSize64 && size != EVEImageSize128 && size != EVEImageSize256) {
+	/*if (size != EVEImageSize32 && size != EVEImageSize64 && size != EVEImageSize128 && size != EVEImageSize256) {
 		if (errorPtr)
 			*errorPtr = [NSError errorWithDomain:EVEOnlineErrorDomain code:EVEErrorCodeInvalidImageSize userInfo:[NSDictionary dictionaryWithObject:EVEErrorCodeInvalidImageSizeText forKey:NSLocalizedDescriptionKey]];
 		return nil;
 	}
-	else
-		return [NSURL URLWithString:[NSString stringWithFormat:@"http://image.eveonline.com/Corporation/%d_%d.png", corporationID, size]];
+	else*/
+		return [NSURL URLWithString:[NSString stringWithFormat:@"http://image.eveonline.com/Corporation/%d_%d.png", corporationID, [self imageDimensionWithSize:size]]];
 }
 
 + (NSURL*) allianceLogoURLWithAllianceID: (NSInteger) allianceID size: (EVEImageSize) size error:(NSError **)errorPtr {
-	if (size != EVEImageSize32 && size != EVEImageSize64 && size != EVEImageSize128) {
+	/*if (size != EVEImageSize32 && size != EVEImageSize64 && size != EVEImageSize128) {
 		if (errorPtr)
 			*errorPtr = [NSError errorWithDomain:EVEOnlineErrorDomain code:EVEErrorCodeInvalidImageSize userInfo:[NSDictionary dictionaryWithObject:EVEErrorCodeInvalidImageSizeText forKey:NSLocalizedDescriptionKey]];
 		return nil;
 	}
-	else
-		return [NSURL URLWithString:[NSString stringWithFormat:@"http://image.eveonline.com/Alliance/%d_%d.png", allianceID, size]];
+	else*/
+		return [NSURL URLWithString:[NSString stringWithFormat:@"http://image.eveonline.com/Alliance/%d_%d.png", allianceID, [self imageDimensionWithSize:size]]];
 }
 
 + (id) characterPortraitImageWithCharacterID: (NSInteger) characterID size: (EVEImageSize) size error:(NSError **)errorPtr {
@@ -55,6 +58,19 @@
 + (id) allianceLogoImageWithAllianceID: (NSInteger) allianceID size: (EVEImageSize) size error:(NSError **)errorPtr {
 	NSData *data = [NSData dataWithContentsOfURL:[self allianceLogoURLWithAllianceID:allianceID size:size error:errorPtr]];
 	return [[EVEImage alloc] initWithData:data];
+}
+
+#pragma mark - Private
+
++ (NSInteger) imageDimensionWithSize:(EVEImageSize) size {
+	static NSInteger scale = 0;
+	if (scale == 0)
+		scale = [[UIScreen mainScreen] scale];
+	
+	BOOL retina = (size & EVEImageSizeRetina) == EVEImageSizeRetina && scale == 2;
+	
+	NSInteger dimension = size & 0xfff;
+	return retina ? dimension * 2 : dimension;
 }
 
 @end
