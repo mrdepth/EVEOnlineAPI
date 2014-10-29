9c41f4da41afda3444dc8bbdaa733e5a5f8deaed
Shimanski Artem <shimanski.artem@gmail.com>
2014-10-29 20:52:50 +0300

    Fixed variables types

--- Classes/RSS/RSS/Parsers/RSSParserAtom.m
+++ Classes/RSS/RSS/Parsers/RSSParserAtom.m
@@ -80,14 +80,14 @@
 	else if ([elementName compare:@"author" options:NSCaseInsensitiveSearch] == NSOrderedSame) {
 		if ([object respondsToSelector:@selector(setAuthor:)]) {
 			NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[object methodSignatureForSelector:@selector(setAuthor:)]];
-			[object setAuthor:[NSURL URLWithString:[attributeDict valueForKey:@"href"]]];
+			[object setAuthor:[attributeDict valueForKey:@"href"]];
 			[invocation setTarget:object];
 			[invocation setSelector:@selector(setAuthor:)];
 			[self.stack addObject:invocation];
 		}
 		else if ([object respondsToSelector:@selector(setPublisher:)]) {
 			NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[object methodSignatureForSelector:@selector(setPublisher:)]];
-			[object setPublisher:[NSURL URLWithString:[attributeDict valueForKey:@"href"]]];
+			[object setPublisher:[attributeDict valueForKey:@"href"]];
 			[invocation setTarget:object];
 			[invocation setSelector:@selector(setPublisher:)];
 			[self.stack addObject:invocation];
@@ -188,21 +188,3 @@
 }
 
 @end
-
-
-/*
-<entry><id>tag:blogger.com,1999:blog-7252484281098761628.post-5859867541985419932</id>
-<published>2011-09-08T05:43:00.000-07:00</published>
-<updated>2011-09-08T05:44:48.174-07:00</updated>
-<title type='text'>Contracts Section</title>
-<content type='html'>&lt;div dir="ltr" style="text-align: left;" trbidi="on"&gt;&lt;div class="separator" style="clear: both; text-align: center;"&gt;&lt;a href="http://2.bp.blogspot.com/-hHjbl1wtjT0/Tmi3K_d_VRI/AAAAAAAAADA/m3SFhT50UqE/s1600/Untitled.png" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"&gt;&lt;img border="0" height="240" src="http://2.bp.blogspot.com/-hHjbl1wtjT0/Tmi3K_d_VRI/AAAAAAAAADA/m3SFhT50UqE/s320/Untitled.png" width="320" /&gt;&lt;/a&gt;&lt;a href="http://4.bp.blogspot.com/-k76qxJlr0MA/Tmi3PlVVk1I/AAAAAAAAADE/2OMuZBJXRWE/s1600/Untitled+2.png" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"&gt;&lt;img border="0" height="240" src="http://4.bp.blogspot.com/-k76qxJlr0MA/Tmi3PlVVk1I/AAAAAAAAADE/2OMuZBJXRWE/s320/Untitled+2.png" width="320" /&gt;&lt;/a&gt;&lt;/div&gt;&lt;br /&gt;&lt;div class="separator" style="clear: both; text-align: center;"&gt;&lt;img border="0" height="320" src="http://3.bp.blogspot.com/-FkLK9EgrMOk/Tmi3QgJ9jrI/AAAAAAAAADI/sPk83aLDgWY/s320/Untitled+3.png" width="213" /&gt;&amp;nbsp; &lt;img border="0" height="320" src="http://4.bp.blogspot.com/-mwyzeqR5KVs/Tmi3RlAeMZI/AAAAAAAAADM/DmEghNexRsI/s320/Untitled+4.png" width="213" /&gt;&lt;/div&gt;&lt;br /&gt;&lt;br /&gt;&lt;br /&gt;&lt;/div&gt;&lt;div class="blogger-post-footer"&gt;&lt;img width='1' height='1' src='https://blogger.googleusercontent.com/tracker/7252484281098761628-5859867541985419932?l=www.eveuniverseiphone.com' alt='' /&gt;&lt;/div&gt;
-</content>
-<link rel='replies' type='application/atom+xml' href='http://www.eveuniverseiphone.com/feeds/5859867541985419932/comments/default' title='Post Comments'/>
-<link rel='replies' type='text/html' href='http://www.eveuniverseiphone.com/2011/09/contracts-section.html#comment-form' title='5 Comments'/>
-<link rel='edit' type='application/atom+xml' href='http://www.blogger.com/feeds/7252484281098761628/posts/default/5859867541985419932'/>
-<link rel='self' type='application/atom+xml' href='http://www.blogger.com/feeds/7252484281098761628/posts/default/5859867541985419932'/>
-<link rel='alternate' type='text/html' href='http://www.eveuniverseiphone.com/2011/09/contracts-section.html' title='Contracts Section'/>
-<author><name>Artem Shimanski</name><uri>http://www.blogger.com/profile/05241437057183694626</uri><email>noreply@blogger.com</email></author>
-<media:thumbnail xmlns:media='http://search.yahoo.com/mrss/' url='http://2.bp.blogspot.com/-hHjbl1wtjT0/Tmi3K_d_VRI/AAAAAAAAADA/m3SFhT50UqE/s72-c/Untitled.png' height='72' width='72'/>
-<thr:total>5</thr:total></entry>
-*/
