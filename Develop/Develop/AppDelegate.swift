8ce06d53d388bc515a7fb717fdcb8f846541c202
Shimanski Artem <shimanski.artem@gmail.com>
2019-11-16 17:41:54 +0300

    Initial Combine support

--- Develop/Develop/AppDelegate.swift
+++ Develop/Develop/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import EVEAPI
 import Alamofire
 import Futures
-
+import Combine
 
 
 let clientID = "a0cc80b7006944249313dc22205ec645"
@@ -35,7 +35,313 @@ class MyCache: URLCache {
 		super.storeCachedResponse(other, for: request)
 	}
 }
+/*
+protocol ResponseDecoder {
+    associatedtype Success
+    associatedtype Failure: Error
+    associatedtype RequestType: Request
+    
+    func response(from request: RequestType, completion: @escaping (Result<Success, Failure>) -> Void) -> RequestType
+}
 
+struct DecodableResponseDecoder<Success>: ResponseDecoder where Success: Decodable {
+    var queue: DispatchQueue = .main
+    var decoder: DataDecoder = JSONDecoder()
+    
+    func response(from request: DataRequest, completion: @escaping (Result<Success, AFError>) -> Void) -> DataRequest {
+        request.responseDecodable(of: Success.self, queue: queue, decoder: decoder) { response in
+            completion(response.result)
+        }
+    }
+}
+
+struct StringResponseDecoder: ResponseDecoder {
+    func response(from request: DataRequest, completion: @escaping (Result<String, AFError>) -> Void) -> DataRequest {
+        request.responseString { response in
+            completion(response.result)
+        }
+    }
+}
+
+class RequestPublisher<Decoder: ResponseDecoder>: Publisher {
+    typealias Output = Decoder.Success
+    typealias Failure = Decoder.Failure
+    typealias RequestType = Decoder.RequestType
+    
+    private class Subscription<SubscriberType: Subscriber>: Combine.Subscription where SubscriberType.Input == Output, SubscriberType.Failure == Failure {
+        var publisher: RequestPublisher?
+        var subscriber: SubscriberType?
+        
+        init(publisher: RequestPublisher, subscriber: SubscriberType) {
+            self.publisher = publisher
+            self.subscriber = subscriber
+        }
+        
+        func request(_ demand: Subscribers.Demand) {
+            publisher?.request(self) { [weak self] result in
+                switch result {
+                case let .success(value):
+                    if demand > 0 {
+                        _ = self?.subscriber?.receive(value)
+                    }
+                    self?.subscriber?.receive(completion: .finished)
+                case let .failure(error):
+                    self?.subscriber?.receive(completion: .failure(error))
+                }
+            }
+        }
+        
+        func cancel() {
+            publisher?.cancel(self)
+            publisher = nil
+            subscriber = nil
+        }
+    }
+    
+    func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        subscriber.receive(subscription: Subscription(publisher: self, subscriber: subscriber))
+    }
+    
+    private var request: RequestType!
+    private let lock = NSLock()
+
+    init(request: RequestType, decoder: Decoder) {
+        self.request = decoder.response(from: request) { [weak self] (result) in
+            self?.lock.lock()
+            defer {self?.lock.unlock()}
+            self?.result = result
+            let requests = self?.requests
+            self?.requests.removeAll()
+            requests?.values.forEach {$0(result)}
+        }
+    }
+    
+    private var requests: [CombineIdentifier: (Result<Output, Failure>) -> Void] = [:]
+    private var result: Result<Output, Failure>?
+    
+    private func request<S: Combine.Subscription>(_ subscription: S, block: @escaping (Result<Output, Failure>) -> Void ) {
+        lock.lock()
+        defer{lock.unlock()}
+        if let result = result {
+            block(result)
+        }
+        else {
+            requests[subscription.combineIdentifier] = block
+            if request.state == .initialized {
+                request.resume()
+            }
+        }
+    }
+    
+    deinit {
+        request.cancel()
+    }
+    
+    private func cancel<S: Combine.Subscription>(_ subscription: S) {
+        lock.lock()
+        defer{lock.unlock()}
+        requests[subscription.combineIdentifier] = nil
+        request.cancel()
+    }
+}
+
+extension DataRequest {
+    func stringPublisher() -> RequestPublisher<StringResponseDecoder> {
+        return RequestPublisher(request: self, decoder: StringResponseDecoder())
+    }
+}
+*/
+
+extension Publisher where Output: DataRequest, Failure == AFError {
+    func validate(_ validation: @escaping DataRequest.Validation) -> Validate<Self> {
+        Validate(upstream: self, validation: validation)
+    }
+    
+    func decode<Item: Decodable>(type: Item.Type, decoder: DataDecoder = JSONDecoder()) -> Decode<Self, Item> {
+        Decode(upstream: self, decoder: decoder)
+    }
+}
+
+public struct Validate<Upstream> : Publisher where Upstream : Publisher, Upstream.Output: DataRequest, Upstream.Failure == AFError {
+
+    public typealias Failure = Upstream.Failure
+    public typealias Output = Upstream.Output
+    public let upstream: Upstream
+
+    /// The closure that transforms elements from the upstream publisher.
+    public let validation: DataRequest.Validation
+
+    public init(upstream: Upstream, validation: @escaping DataRequest.Validation) {
+        self.upstream = upstream
+        self.validation = validation
+    }
+    
+    class Inner<Downstream: Subscriber>: Subscription, Subscriber where Downstream.Input == Validate.Output, Downstream.Failure == Validate.Failure {
+        typealias Input = Upstream.Output
+        typealias Failure = Upstream.Failure
+
+        var downstream: Downstream?
+        var upstream: Subscription?
+        var validation: DataRequest.Validation
+        
+        init(_ subscriber: Downstream, validation block: @escaping DataRequest.Validation) {
+            downstream = subscriber
+            validation = block
+        }
+
+        func receive(subscription: Subscription) {
+            upstream = subscription
+            downstream?.receive(subscription: self)
+        }
+        
+        func receive(_ input: Upstream.Output) -> Subscribers.Demand {
+            downstream?.receive(input.validate(validation)) ?? .none
+        }
+        
+        func receive(completion: Subscribers.Completion<Failure>) {
+            downstream?.receive(completion: completion)
+        }
+
+        func request(_ demand: Subscribers.Demand) {
+            upstream?.request(demand)
+        }
+        
+        func cancel() {
+            upstream?.cancel()
+            downstream = nil
+            upstream = nil
+        }
+        
+    }
+    
+    public func receive<S>(subscriber: S) where Output == S.Input, S : Subscriber, Upstream.Failure == S.Failure {
+        let inner = Inner(subscriber, validation: validation)
+        upstream.receive(subscriber: inner)
+    }
+}
+
+public struct DataRequestPublisher: Publisher {
+    public typealias Output = DataRequest
+    public typealias Failure = AFError
+    public var session: Session
+    let url: URLConvertible
+    let method: HTTPMethod
+    let parameters: Parameters?
+    let encoding: ParameterEncoding
+    let headers: HTTPHeaders?
+
+    public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        let request = session.request(url, method: method, parameters: parameters, encoding: encoding, headers: headers)
+        let subscription = Subscription(downstream: subscriber, request: request)
+        subscriber.receive(subscription: subscription)
+    }
+    
+    class Subscription<Downstream: Subscriber>: Combine.Subscription where Downstream.Input == Output, Downstream.Failure == Failure {
+        var request: Output
+        var downstream: Downstream?
+        
+        init(downstream: Downstream, request: Output) {
+            self.downstream = downstream
+            self.request = request
+        }
+
+        func request(_ demand: Subscribers.Demand) {
+            guard demand > 0 else {return}
+            _ = downstream?.receive(request)
+        }
+        
+        func cancel() {
+            downstream = nil
+            guard request.state == .resumed else {return}
+            request.cancel()
+        }
+        
+    }
+    
+}
+
+public struct Decode<Upstream, Output> : Publisher where Upstream: Publisher, Output: Decodable, Upstream.Output: DataRequest, Upstream.Failure == AFError {
+    public typealias Failure = Upstream.Failure
+    public let upstream: Upstream
+    public let decoder: DataDecoder
+    
+    public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        let subscription = Inner(subscriber, queue: .main, decoder: decoder)
+        upstream.receive(subscriber: subscription)
+    }
+    
+    class Inner<Downstream: Subscriber>: Subscription, Subscriber where Downstream.Input == Decode.Output, Downstream.Failure == Decode.Failure {
+        typealias Input = Upstream.Output
+        typealias Failure = Upstream.Failure
+
+        var downstream: Downstream?
+        var upstream: Subscription?
+        var decoder: DataDecoder
+        var queue: DispatchQueue
+        
+        init(_ subscriber: Downstream, queue: DispatchQueue, decoder: DataDecoder) {
+            downstream = subscriber
+            self.decoder = decoder
+            self.queue = queue
+        }
+
+        func receive(subscription: Subscription) {
+            upstream = subscription
+            downstream?.receive(subscription: self)
+        }
+        
+        func receive(_ input: Upstream.Output) -> Subscribers.Demand {
+            let r = input.responseDecodable(of: Output.self, queue: queue, decoder: decoder) { (response) in
+                switch response.result {
+                case let .success(value):
+                    _ = self.downstream?.receive(value)
+                    self.downstream?.receive(completion: .finished)
+                case let .failure(error):
+                    self.downstream?.receive(completion: .failure(error))
+                }
+                
+            }
+            if r.state == .initialized {
+                r.resume()
+            }
+            
+            return .none
+        }
+        
+        func receive(completion: Subscribers.Completion<Failure>) {
+            downstream?.receive(completion: completion)
+        }
+
+        func request(_ demand: Subscribers.Demand) {
+            upstream?.request(demand)
+        }
+        
+        func cancel() {
+            upstream?.cancel()
+            downstream = nil
+            upstream = nil
+        }
+    }
+}
+
+extension Session {
+    func publisher(_ convertible: URLConvertible,
+                   method: HTTPMethod = .get,
+                   parameters: Parameters? = nil,
+                   encoding: ParameterEncoding = URLEncoding.default,
+                   headers: HTTPHeaders? = nil,
+                   interceptor: RequestInterceptor? = nil) -> DataRequestPublisher {
+        
+        DataRequestPublisher(session: self, url: convertible, method: method, parameters: parameters, encoding: encoding, headers: headers)
+    }
+
+    
+}
+
+struct Time: Codable {
+    var currentDateTime: String
+    var utcOffset: String
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -46,106 +352,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		case dateFormatError
 	}
 
+    let session = Session(startRequestsImmediately: false)
+    var c1: AnyCancellable?
+    var c2: AnyCancellable?
+    var c3: AnyCancellable?
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		URLCache.shared = MyCache(memoryCapacity: 1024*1024, diskCapacity: 50*1024*1024, diskPath: nil)
-        var esi: ESI? = ESI()
-        let request = esi?.status.retrieveTheUptimeAndPlayerCounts().then(on: .main) { result in
-            print(result)
-        }.catch{ error in
-                print(error)
-        }.finally {
-            print(#function)
+        let p = URLSession.shared.dataTaskPublisher(for: URL(string: "http://worldclockapi.com/api/json/est/now")!)
+        c1 = p.sink(receiveCompletion: { (c) in
+            print(c)
+        }) { (d) in
+            print(d)
         }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            esi = nil
-            print(request)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.c2 = p.sink(receiveCompletion: { (c) in
+                print(c)
+            }) { (d) in
+                print(d)
+            }
         }
-		
-//        AF.request("http://google.com").responseString { result in
+        c3 = session.publisher("http://worldclockapi.com/api/json/est/now").decode(type: Time.self)
+            .sink(receiveCompletion: { (c) in
+                print(c)
+            }) { (d) in
+                print(d)
+            }
+//        let r = session.request("http://google.com").validate().stringPublisher().catch{_ in Just("")}.sink { result in
 //            print(result)
 //        }
-		
-		/*var esi: ESI! = ESI()
-		esi.request("https://esi.evetech.net/latest/alliances/?datasource=tranquility", cachePolicy: .useProtocolCachePolicy).responseString { (r) in
-			print(r)
-			esi = nil
-		}
-		
-		var zkb: ZKillboard! = ZKillboard()
-		zkb.kills(filter: [.solo, .wSpace], page: nil).then {
-			print($0)
-		}.catch {
-			print($0)
-		}.finally {
-			zkb = nil
-		}*/
-		
-//		let url = OAuth2.authURL(clientID: clientID, callbackURL: URL(string:"eveauthnc://sso/")!, scope: ESI.Scope.default, state: "esi")
-//		application.open(url, options: [:], completionHandler: nil)
-		
-	/*	DispatchQueue.global(qos: .background).async {
-			print("p1")
-			try! p1.set(.success(1))
-		}
-		
-		p1.future.then { _ in
-			print("p22")
-		}
-
-		p1.future.then { _ in
-			print("p2")
-			try! p2.set(.success(2))
-		}
-
-
-		let v = try? p2.future.get()
-		print("wait")
-		p2.future.then { r in
-			print("\(r)")
-		}*/
-		
-		return true
-		/*let s = "{\"scopes\":[\"characterWalletRead\",\"esi-calendar.respond_calendar_events.v1\",\"esi-calendar.read_calendar_events.v1\",\"esi-location.read_location.v1\",\"esi-location.read_ship_type.v1\",\"esi-mail.organize_mail.v1\",\"esi-mail.read_mail.v1\",\"esi-mail.send_mail.v1\",\"esi-skills.read_skills.v1\",\"esi-skills.read_skillqueue.v1\",\"esi-wallet.read_character_wallet.v1\",\"esi-search.search_structures.v1\",\"esi-clones.read_clones.v1\",\"esi-universe.read_structures.v1\",\"esi-killmails.read_killmails.v1\",\"esi-assets.read_assets.v1\",\"esi-planets.manage_planets.v1\",\"esi-fittings.read_fittings.v1\",\"esi-fittings.write_fittings.v1\",\"esi-markets.structure_markets.v1\",\"esi-characters.read_loyalty.v1\",\"esi-characters.read_standings.v1\",\"esi-industry.read_character_jobs.v1\",\"esi-markets.read_character_orders.v1\",\"esi-characters.read_blueprints.v1\",\"esi-contracts.read_character_contracts.v1\",\"esi-clones.read_implants.v1\"],\"characterID\":90553786,\"characterName\":\"Po\'kupatel Boloskarl\",\"refreshToken\":\"4AhefRhvXaUdqskQjF_ZFcHQnSW-PJuUB796LhkKJanORK9jd63ESBz241X7uSGVxdwceVD5Rm08gCQGAzqpbKmLDbVWBberwfegJvZYBQUJ27rjIwOlqK9maCQUknudzzmhmVbgajTcYVuKruU7kJbQeTITWf19FjCPbLYBf5Di79NJyiI0Z2fAmYQKR7Uh0\",\"tokenType\":\"Bearer\",\"expiresOn\":537274087.78973496,\"accessToken\":\"KBohnlYQ7fYKkuzacT-n1tFZ4d4PVuxjiUNL2eTswtpcIRKvXvgbC7CM-sekvpphjjHizYrOEowWg-abwMxTNQ2\",\"realm\":\"esi\"}"
-		let data = s.data(using: .utf8)!
-		let token = try! JSONDecoder().decode(OAuth2Token.self, from: data)
-		
-		/*var retrier: OAuth2Retrier? = OAuth2Retrier(token: token, clientID: clientID, secretKey: secretKey)
-		print("\(token.refreshToken)")
-		print("\(token.accessToken)")
-		retrier?.refreshToken { (result) in
-			print("\(result)")
-			print("\(token.refreshToken)")
-			print("\(token.accessToken)")
-			retrier = nil
-		}
-		
-		let url = OAuth2.authURL(clientID: clientID, callbackURL: URL(string:"eveauthnc://sso/")!, scope: ESI.Scope.default, state: "esi")
-//		application.open(url, options: [:], completionHandler: nil)*/
-
-		var esi: ESI? = ESI(token: token, clientID: clientID, secretKey: secretKey)
-		let dispatchGroup = DispatchGroup()
-		dispatchGroup.enter()
-		esi?.wallet.getCharactersWalletBalance(characterID: Int(token.characterID), completionBlock: { (result) in
-			dispatchGroup.leave()
-		})
-		
-		dispatchGroup.enter()
-		esi?.skills.getCharacterAttributes(characterID: Int(token.characterID), completionBlock: { (result) in
-			dispatchGroup.leave()
-		})
-		
-		dispatchGroup.enter()
-		esi?.skills.getCharactersSkillQueue(characterID: Int(token.characterID), completionBlock: { (result) in
-			dispatchGroup.leave()
-			
-		})
-		
-		dispatchGroup.notify(queue: .main) {
-			esi = nil
-		}*/
-		return true
-	}
+//        c = r
+        return true
+    }
 
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -169,14 +407,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
 	
-	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-		return OAuth2.handleOpenURL(url, clientID: clientID, secretKey: secretKey) { (result) in
-			guard let token = result.value else {return}
-			let s = String(data: try! JSONEncoder().encode(token), encoding: .utf8)
-			print(s!)
-		}
-	}
-
-
+//	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+//		return OAuth2.handleOpenURL(url, clientID: clientID, secretKey: secretKey) { (result) in
+//			guard let token = result.value else {return}
+//			let s = String(data: try! JSONEncoder().encode(token), encoding: .utf8)
+//			print(s!)
+//		}
+//	}
 }
 
